dac8da5f4264f00c4def73ad22bd61744bcdc750
deepak singh <deepak11singh98@gmail.com>
2023-01-20 21:55:57 +0530

    http

--- dashboard/src/actions/baseURL.js
+++ dashboard/src/actions/baseURL.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://65.2.126.223:4000",
-  baseURL: "http://localhost:4000",
+  baseURL: "http://65.2.126.223:4000",
+  // baseURL: "http://localhost:4000",
   withCredentials: true,
 });
 
--- server/backend/utils/jwtToken.js
+++ server/backend/utils/jwtToken.js
@@ -8,6 +8,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     secure: true,
     sameSite: "None",
+    httpOnly: true,
   };
   // console.log(`token ${token}`);
   // console.log(`user ${user}`);
