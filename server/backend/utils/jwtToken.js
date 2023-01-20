aaf8fd35893aba1b4292263ec7abba0cafbad0fd
deepak singh <deepak11singh98@gmail.com>
2023-01-20 21:52:08 +0530

    http

--- server/backend/utils/jwtToken.js
+++ server/backend/utils/jwtToken.js
@@ -6,7 +6,8 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    SameSite: "None",
+    secure: true,
+    sameSite: "None",
   };
   // console.log(`token ${token}`);
   // console.log(`user ${user}`);
